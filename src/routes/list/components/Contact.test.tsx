7ebbae530c4e7a1afaf0e23d0bd4b672a6c6a4ff
Endy Gower <endygower@outlook.com>
2023-11-13 02:43:54 +0800

    contact test file

---
+++ src/routes/list/components/Contact.test.tsx
@@ -0,0 +1,63 @@
+import { PropsWithChildren } from 'react'
+import { HashRouter } from 'react-router-dom'
+import { describe, expect, it } from 'vitest'
+
+import { MockedProvider } from '@apollo/client/testing'
+import { render, screen } from '@testing-library/react'
+
+import Contact from './Contact'
+import { afterEach } from 'node:test'
+import { FavoriteProvider } from '../contexts/favorite'
+
+function Providers(props: PropsWithChildren) {
+  return (
+    <HashRouter>
+      <MockedProvider>
+        <FavoriteProvider>{props.children}</FavoriteProvider>
+      </MockedProvider>
+    </HashRouter>
+  )
+}
+
+describe('App', () => {
+  const mock = {
+    id: 1,
+    first_name: 'lorem',
+    last_name: 'ipsum',
+    phones: [{ number: 'my phone number' }],
+  }
+const addLabel = 'Add lorem ipsum to favorite'
+const removeLabel = 'Remove lorem ipsum from favorite'
+
+  afterEach(() => {
+    localStorage.clear()
+  })
+
+  it('renders correctly', () => {
+    render(<Contact contact={mock} />, { wrapper: Providers })
+
+    const name = screen.getByText('lorem ipsum')
+    expect(name).toBeInTheDocument()
+
+    const phones = screen.getByText('my phone number')
+    expect(phones).toBeInTheDocument()
+
+    const addTofavorite = screen.getByLabelText(addLabel)
+    expect(addTofavorite).toBeInTheDocument()
+
+    const removeFromfavorite = screen.queryByLabelText(removeLabel)
+    expect(removeFromfavorite).not.toBeInTheDocument()
+  })
+
+  it('renders remove from favorite action', () => {
+    localStorage.setItem('favorite', JSON.stringify([1]))
+
+    render(<Contact contact={mock} />, { wrapper: Providers })
+
+    const addTofavorite = screen.queryByLabelText(addLabel)
+    expect(addTofavorite).not.toBeInTheDocument()
+
+    const removeFromfavorite = screen.getByLabelText(removeLabel)
+    expect(removeFromfavorite).toBeInTheDocument()
+  })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
@@ -5,6 +6,11 @@ import path from 'node:path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./testSetup.ts'],
+    globals: true,
+  },
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './src/'),
